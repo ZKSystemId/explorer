62ece757b1054104bc6a10f6843a936455215de5
ZKSystemId <123727655+ZKSystemId@users.noreply.github.com>
2023-01-27 21:28:55 +0700

    Update themeConfig.js

--- themeConfig.js
+++ themeConfig.js
@@ -11,7 +11,7 @@ export const $themeConfig = {
   app: {
     appName: 'ZKSystem', // Will update name in navigation menu (Branding)
     // eslint-disable-next-line global-require
-    appLogoImage: '/logo1.svg', // Will update logo in navigation menu (Branding)
+    appLogoImage: '/public/logo1.svg', // Will update logo in navigation menu (Branding)
   },
   layout: {
     isRTL: false,
