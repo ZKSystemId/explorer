09839b67cd001281984d8bab0212a4e546060afa
ZKSystemId <123727655+ZKSystemId@users.noreply.github.com>
2023-01-28 01:10:24 +0700

    Update index.js

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -19,7 +19,7 @@ function processMenu() {
       title: 'blockchains',
       children: blockchains,
       tag: `${blockchains.length}`,
-      icon: 'https://explorer.zksystem.my.id/logo.svg',
+      icon: 'https://explorer.zksystem.my.id/logox.svg',
     })
   }
   chainMenus.push({ header: 'LINKS' })
