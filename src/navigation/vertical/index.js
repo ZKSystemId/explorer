2b8fbd6bc27c5f0a508a480f319ffe31d70e4c12
ZKSystemId <123727655+ZKSystemId@users.noreply.github.com>
2023-02-08 09:51:40 +0700

    Update index.js

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -46,23 +46,27 @@ function processMenu() {
     href: 'https://github.com/ZKSystemId/',
     icon: 'GithubIcon',
   })
-  chainMenus.push({ header: 'PLANQ TOOLS' })
+  if (adv.length > 1) {
+  chainMenus.push({ header: 'Advanced' })
   chainMenus.push({
     title: 'Chart',
+    tag: `${adv.length}`,
     href: 'https://cosmospug.com/planq-chart/',
     icon: 'ChromeIcon',
   })
   chainMenus.push({
     title: 'Price',
+    tag: `${adv.length}`,
     href: 'https://dexscreener.com/osmosis/898',
     icon: 'ChromeIcon',
   })
   chainMenus.push({
     title: 'Trade',
+    tag: `${adv.length}`,
     href: 'https://frontier.osmosis.zone/pool/898',
     icon: 'LifeBuoyIcon',
   })
-
+}
   return chainMenus
 }
 
